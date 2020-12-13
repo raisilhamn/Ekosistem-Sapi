9bf4d3d419ce4b4855219402686ec327f784bcf6
rais ilham <raispinter12@gmail.com>
2020-12-14 05:08:24 +0700

    Update OOP pada mean dan varian

--- src/Perhitungan.java
+++ src/Perhitungan.java
@@ -3,13 +3,29 @@ import java.util.ArrayList;
 public class Perhitungan {
 
 
-
-    public int reset(int a) {
-        a = 0;
-        return a;
+    public static double Mean(ArrayList<ArrayList<Double>> data, int i) {
+        double jumlah = 0.0;
+        ArrayList<Double> dataA;
+        dataA = data.get(i);
+        for (int j = 0; j < dataA.size(); j++) {
+            jumlah += dataA.get(j);
+        }
+        return jumlah / dataA.size();
     }
-    public void display(int a) {
-        System.out.println(a);
+
+    public static double varian(ArrayList<ArrayList<Double>> data, int i, double mean) {
+        double sigma = 0.0;
+        ArrayList<Double> dataA;
+        dataA = data.get(i);
+        for (int k = 0; k < dataA.size(); k++) {
+            sigma = dataA.get(k) - mean;
+            sigma += sigma;
+        }
+        return (Math.sqrt((sigma * sigma) / dataA.size()));
+    }
+
+    public int reset() {
+        return 0;
     }
 
 }
